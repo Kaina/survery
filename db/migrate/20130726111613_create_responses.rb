48ef472e63876d7ce05bf9d4835f90cf34eac988
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-26 11:31:26 -0500

    migrations complete model shells

--- db/migrate/20130726111613_create_responses.rb
+++ db/migrate/20130726111613_create_responses.rb
@@ -2,7 +2,11 @@ class CreateResponses < ActiveRecord::Migration
   def change
       create_table :questions do |t|
       t.string :response_string
+      t.integer :response_integer
+      t.boolean :response_boolean
       t.integer :survey_id
+      t.integer :choice_id
+      t.integer :user_id
  
       t.timestamps
     end
